eebdad62f545707735f4d63cbab7876a47ab101b
Bauti Revel <tu_mail_de_github@ejemplo.com>
2026-07-14 21:26:55 -0300

    Simular rechazo de pago por limite en tarjetas para activar PagoRechazadoException

--- src/com/ecommerce/pagos/PagoTarjetaCredito.java
+++ src/com/ecommerce/pagos/PagoTarjetaCredito.java
@@ -1,6 +1,12 @@
 package com.ecommerce.pagos;
 public class PagoTarjetaCredito implements ProcesadorPago {
+    private static final double LIMITE = 5_000_000;
+
     @Override public boolean procesarPago(double monto) {
+        if (monto > LIMITE) {
+            System.out.println("Procesando pago de $" + monto + " con Tarjeta de Credito... [RECHAZADO: excede el limite de la tarjeta]");
+            return false;
+        }
         System.out.println("Procesando pago de $" + monto + " con Tarjeta de Credito... ¡Aprobado!");
         return true;
     }
--- src/com/ecommerce/pagos/PagoTarjetaDebito.java
+++ src/com/ecommerce/pagos/PagoTarjetaDebito.java
@@ -1,6 +1,12 @@
 package com.ecommerce.pagos;
 public class PagoTarjetaDebito implements ProcesadorPago {
+    private static final double LIMITE = 2_000_000;
+
     @Override public boolean procesarPago(double monto) {
+        if (monto > LIMITE) {
+            System.out.println("Procesando pago de $" + monto + " con Tarjeta de Debito... [RECHAZADO: excede el limite diario]");
+            return false;
+        }
         System.out.println("Procesando pago de $" + monto + " con Tarjeta de Debito... ¡Aprobado!");
         return true;
     }
